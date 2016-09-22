f86f3206b8b3bf4f1af155c857d1699e5caad874
frank.zheng <frank.zheng@alo7.com>
2016-09-22 13:07:48 +0800

    Add model patient & location

---
+++ db/migrate/20160922044147_create_patients.rb
@@ -0,0 +1,17 @@
+class CreatePatients < ActiveRecord::Migration
+  def change
+    create_table :patients do |t|
+      t.string :first_name,      null: false
+      t.string :middle_name
+      t.string :last_name,       null: false
+      t.datetime :date_of_birth, null: false
+      t.string :gender,          null: false
+      t.string :status,          null: false
+      t.integer :location_id,    index: true
+      t.integer :viewed_count,   null: false, default: 0
+      t.boolean :is_deleted,     null: false, default: false
+
+      t.timestamps               null: false
+    end
+  end
+end
