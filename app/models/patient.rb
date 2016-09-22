1cc66e08ee4885a9209819b594403dc28f8681d7
frank.zheng <frank.zheng@alo7.com>
2016-09-22 19:34:02 +0800

    Add patient # age, full_name, medic_record

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,18 @@
 class Patient < ActiveRecord::Base
   belongs_to :location
+
+  MEDIC_RECORD_CODE_LENGTH = 6
+  def full_name
+    "#{last_name}, #{first_name} #{middle_name}"
+  end
+
+  def medic_record
+    base_str = "MR"
+    (MEDIC_RECORD_CODE_LENGTH - id.to_s.length).times { base_str << "0" }
+    "#{base_str}#{id}"
+  end
+
+  def age
+    ((Time.now - date_of_birth) / 1.years).to_i
+  end
 end
--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Patient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "About Dynamic Properties" do
+    before do
+      @patient1 = Patient.new(
+        id: 222,
+        first_name: "a",
+        middle_name: "b",
+        last_name: "c",
+        date_of_birth: Time.now - 20.years,
+        gender: "male",
+        status: "initial"
+      )
+      @patient2 = Patient.new(id: 1234567)
+    end
+
+    it "should return patient's full name" do
+      @patient1.full_name.should === "c, a b"
+    end
+
+    it "should return patient's MR" do
+      @patient1.medic_record.should === "MR000222"
+      @patient2.medic_record.should === "MR1234567"
+    end
+
+    it "should return patient's age" do
+       @patient1.age.should === 20
+    end
+  end
 end
