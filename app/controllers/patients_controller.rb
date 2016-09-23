3b549c6177f194d6c99abad58c93f002a2dca5a4
frank.zheng <frank.zheng@alo7.com>
2016-09-23 15:03:18 +0800

    Add create patient

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,4 +2,30 @@ class PatientsController < BaseController
   def index
     @patients = Patient.all
   end
+
+  def new
+    @patient = Patient.new
+  end
+
+  def create
+    patient_attributes = patient_params
+    Patient.create!(patient_attributes)
+    redirect_to patients_path
+  end
+
+  private
+
+  def patient_params
+    patient_attributes = params.require(:patient).permit(
+      :first_name,
+      :middle_name,
+      :last_name,
+      :gender,
+      :status,
+      :location_id,
+      :date_of_birth
+    )
+    patient_attributes[:date_of_birth] = Time.parse(patient_attributes[:date_of_birth])
+    patient_attributes
+  end
 end
