8f9b89e88b8c9e80e76a32020e34d9a7d2888e68
frank.zheng <frank.zheng@alo7.com>
2016-09-23 17:27:37 +0800

    Add show patient

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,4 +1,6 @@
 class PatientsController < BaseController
+  before_action :find_patient, only: [:show]
+
   def index
     @patients = Patient.all
   end
@@ -9,11 +11,19 @@ class PatientsController < BaseController
 
   def create
     patient_attributes = patient_params
-    Patient.create!(patient_attributes)
-    redirect_to patients_path
+    patient = Patient.create!(patient_attributes)
+    redirect_to patient_path(patient)
+  end
+
+  def show
+    @patient.is_viewed_detail!
   end
 
   private
+
+  def find_patient
+    @patient = Patient.find(params[:id])
+  end
 
   def patient_params
     patient_attributes = params.require(:patient).permit(
