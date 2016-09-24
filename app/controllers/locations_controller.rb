51754870f341d3567067ee67359279d0c4be9d23
frank.zheng <frank.zheng@alo7.com>
2016-09-24 11:28:06 +0800

    Add show location

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,13 @@
 class LocationsController < BaseController
+  before_action :find_location, only: [:show]
+
+  def show
+    @patients = @location.not_deleted_patients
+  end
+
+  private
+
+  def find_location
+    @location = Location.find(params[:id])
+  end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,7 @@
 class Location < ActiveRecord::Base
   has_many :patients
+
+  def not_deleted_patients
+    patients.not_deleted
+  end
 end
